73a4e13aefd7cec39b3e66d75267569516ad7144
m.kaeppler <m.kaeppler@11f29d54-35b8-11de-8e0c-53a27eea117e>
2009-10-28 20:01:06 +0000

    trim base64 encoded signature string (fixes issue 18)

--- signpost-core/src/main/java/oauth/signpost/signature/HmacSha1MessageSigner.java
+++ signpost-core/src/main/java/oauth/signpost/signature/HmacSha1MessageSigner.java
@@ -29,28 +29,28 @@ import oauth.signpost.http.HttpRequest;
 @SuppressWarnings("serial")
 public class HmacSha1MessageSigner extends OAuthMessageSigner {
 
-    private static final String MAC_NAME = "HmacSHA1";
+	private static final String MAC_NAME = "HmacSHA1";
 
-    @Override
-    public String sign(HttpRequest request, Map<String, String> oauthParameters)
-            throws OAuthMessageSignerException {
-        try {
-            String keyString = OAuth.percentEncode(getConsumerSecret()) + '&'
-                    + OAuth.percentEncode(getTokenSecret());
-            byte[] keyBytes = keyString.getBytes(OAuth.ENCODING);
+	@Override
+	public String sign(HttpRequest request, Map<String, String> oauthParameters)
+			throws OAuthMessageSignerException {
+		try {
+			String keyString = OAuth.percentEncode(getConsumerSecret()) + '&'
+					+ OAuth.percentEncode(getTokenSecret());
+			byte[] keyBytes = keyString.getBytes(OAuth.ENCODING);
 
-            SecretKey key = new SecretKeySpec(keyBytes, MAC_NAME);
-            Mac mac = Mac.getInstance(MAC_NAME);
-            mac.init(key);
+			SecretKey key = new SecretKeySpec(keyBytes, MAC_NAME);
+			Mac mac = Mac.getInstance(MAC_NAME);
+			mac.init(key);
 
-            String sbs = computeSignatureBaseString(request, oauthParameters);
-            byte[] text = sbs.getBytes(OAuth.ENCODING);
+			String sbs = computeSignatureBaseString(request, oauthParameters);
+			byte[] text = sbs.getBytes(OAuth.ENCODING);
 
-            return base64Encode(mac.doFinal(text));
-        } catch (GeneralSecurityException e) {
-            throw new OAuthMessageSignerException(e);
-        } catch (UnsupportedEncodingException e) {
-            throw new OAuthMessageSignerException(e);
-        }
-    }
+			return base64Encode(mac.doFinal(text)).trim();
+		} catch (GeneralSecurityException e) {
+			throw new OAuthMessageSignerException(e);
+		} catch (UnsupportedEncodingException e) {
+			throw new OAuthMessageSignerException(e);
+		}
+	}
 }
