19eb19fce16e48bb651eae66e6793be173a90943
Simon Legner <Simon.Legner@gmail.com>
2022-02-15 20:53:55 +0100

    Fix commonshttp5 unit test

--- signpost-commonshttp5/src/test/java/oauth/signpost/commonshttp5/HttpRequestAdapterTest.java
+++ signpost-commonshttp5/src/test/java/oauth/signpost/commonshttp5/HttpRequestAdapterTest.java
@@ -4,8 +4,11 @@ import oauth.signpost.basic.HttpRequestAdapterTestBase;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.core5.http.ContentType;
 import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnit44Runner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnit44Runner.class)
 public class HttpRequestAdapterTest extends HttpRequestAdapterTestBase {
@@ -17,5 +20,11 @@ public class HttpRequestAdapterTest extends HttpRequestAdapterTestBase {
         StringEntity body = new StringEntity(PAYLOAD, ContentType.TEXT_PLAIN);
         r.setEntity(body);
         request = new HttpRequestAdapter(r);
+    }
+
+    @Test
+    @Override
+    public void shouldReturnCorrectContentType() {
+        assertEquals("text/plain; charset=ISO-8859-1", request.getContentType());
     }
 }
